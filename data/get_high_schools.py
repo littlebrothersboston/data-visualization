71e4019af18aa77d0472ae556621a0dfb46baa17
julian-zucker <julian.zucker@gmail.com>
2018-04-14 13:14:15 -0400

    got high schools

--- data/get_high_schools.py
+++ data/get_high_schools.py
@@ -1,16 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+
+url = "https://www.bostonpublicschools.org//site/UserControls/Minibase" + \
+      "/MinibaseListWrapper.aspx?ModuleInstanceID=1824&PageModuleInstanceID=1849" + \
+      "&FilterFields=2%3AE%3A9-12%3B&DirectoryType=C&PageIndex=1&_=1522699936745"
+soup = BeautifulSoup(requests.get(url).content, "html.parser")
+high_schools = soup.find_all("div", "sw-flex-item-group")
+school_divs = [x.find_all("div") for x in high_schools]
+
+out = []
+for school in school_divs:
+    values = [row.get_text() for row in school]
+    out.append({
+        "name": values[0],
+        "principal": values[1],
+        "address": values[2] + " " + values[3],
+        "grades": values[4],
+        "hours": values[5],
+        "type": values[6]
+    })
 
 
-for i in range(10):
-    url = "https://www.bostonpublicschools.org//site/UserControls/Minibase" + \
-          "/MinibaseListWrapper.aspx?ModuleInstanceID=1824&PageModuleInstanceID=1849" + \
-          "&FilterFields=2%3AE%3A9-12%3B&DirectoryType=C&PageIndex=" + str(i) + "&_=1522699936745"
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
-    high_schools = soup.find_all("div", "sw-flex-item-group")
-    spans = [x.find_all("span") for x in high_schools]
-
-    print([y.get_text() for y in spans])
-    # print(high_schools)
-
-    # print(soup.prettify())
+with open("boston_high_schools.json", "w") as file:
+    file.write(json.dumps(out))
